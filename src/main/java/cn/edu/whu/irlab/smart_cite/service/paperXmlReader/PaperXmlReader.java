47079a4cba23356b8c68eee75c2d4dd3ff1fcc5d
carmanzhang <zlahu@foxmail.com>
2020-04-14 21:25:07 +0800

    1. resolve Bug caused by singleton 'Article' 2. test case for Extractor Service, support skip test case when using mvn package
    Signed-off-by: carmanzhang <zlahu@foxmail.com>

--- src/main/java/cn/edu/whu/irlab/smart_cite/service/paperXmlReader/PaperXmlReader.java
+++ src/main/java/cn/edu/whu/irlab/smart_cite/service/paperXmlReader/PaperXmlReader.java
@@ -36,7 +36,9 @@ public class PaperXmlReader {
 
     private static final Logger logger = LoggerFactory.getLogger(PaperXmlReader.class);
 
-    private Article article;
+//    private Article article;
+    ThreadLocal<Article> article = null;
+
 
     /**
      *@auther gcr19
@@ -48,7 +50,7 @@ public class PaperXmlReader {
     public Article processFile(File file, Element root) {
         Element header = root.getChild("header");
 
-        article = new Article(FilenameUtils.getBaseName(file.getName()));
+        Article article = new Article(FilenameUtils.getBaseName(file.getName()));
 
         //初始化article 设置摘要
         article.setAbsText(header.getChild("abstract").getValue());//todo plos数据中有的摘要有多个段落
@@ -87,7 +89,7 @@ public class PaperXmlReader {
 //            logs.info(s.toText());
         }
 
-
+        this.article.set(article);
         return article;
     }
 
@@ -195,7 +197,7 @@ public class PaperXmlReader {
                 String refNum = element.getAttributeValue("rid");
                 if (refNum != null && !refNum.trim().equals("")) {  //指向的参考文献
                     xref.setRid(refNum.trim());
-                    xref.setReference(article.getReferences().get(xref.getRid()));
+                    xref.setReference(article.get().getReferences().get(xref.getRid()));
                 }
                 sentence.addRef(xref);  //给句子加引文引用
                 //引文替换工作
