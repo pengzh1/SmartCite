91a08e489a99ebf17de6642dd700fa1243cb4c2c
gcr19 <gcr1995@126.com>
2020-05-13 00:01:03 +0800

    解决toJson重复调用导致的栈溢出问题

--- src/main/java/cn/edu/whu/irlab/smart_cite/vo/Reference.java
+++ src/main/java/cn/edu/whu/irlab/smart_cite/vo/Reference.java
@@ -3,6 +3,7 @@ package cn.edu.whu.irlab.smart_cite.vo;
 import cn.edu.whu.irlab.smart_cite.util.TypeConverter;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import lombok.Data;
 
 import java.util.ArrayList;
@@ -15,47 +16,51 @@ import java.util.List;
  * @desc 引文实体
  **/
 @Data
-public class Reference
-{
-	private String id="";
-	private String label = "";
-	private List<Author> authors = new ArrayList<>();
-	private String year=""; //发表年份
-	private String article_title=""; //标题
-	private String source=""; //来源
-	private String volume=""; //卷号
-	private String issue; //期号
-	private String fpage=""; //起始页码
-	private String lpage=""; //终止页码
+public class Reference implements ToJsonAble {
+    private String id = "";
+    private String label = "";
+    private List<Author> authors = new ArrayList<>();
+    private String year = ""; //发表年份
+    private String article_title = ""; //标题
+    private String source = ""; //来源
+    private String volume = ""; //卷号
+    private String issue; //期号
+    private String fpage = ""; //起始页码
+    private String lpage = ""; //终止页码
 
-	public void addAuthor(Author author){
-		authors.add(author);
-	}
+    public void addAuthor(Author author) {
+        authors.add(author);
+
+    }
+
+    @Override
+    public String toString() {
+        return "{" + "\"id\":\"" + id + '\"' +
+                ",\"label\":\"" + label + '\"' +
+                ",\"authors\":" + TypeConverter.list2JsonArray(authors) +
+                ",\"year\":\"" + year + '\"' +
+                ",\"article_title\":\"" + article_title + '\"' +
+                ",\"source\":\"" + source + '\"' +
+                ",\"volume\":\"" + volume + '\"' +
+                ",\"issue\":\"" + issue + '\"' +
+                ",\"fpage\":\"" + fpage + '\"' +
+                ",\"lpage\":\"" + lpage + '\"' +
+                '}';
+    }
 
 	@Override
-	public String toString() {
-		final StringBuilder sb = new StringBuilder("{");
-		sb.append("\"id\":\"")
-				.append(id).append('\"');
-		sb.append(",\"label\":\"")
-				.append(label).append('\"');
-		sb.append(",\"authors\":")
-				.append(TypeConverter.list2JsonArray(authors));
-		sb.append(",\"year\":\"")
-				.append(year).append('\"');
-		sb.append(",\"article_title\":\"")
-				.append(article_title).append('\"');
-		sb.append(",\"source\":\"")
-				.append(source).append('\"');
-		sb.append(",\"volume\":\"")
-				.append(volume).append('\"');
-		sb.append(",\"issue\":\"")
-				.append(issue).append('\"');
-		sb.append(",\"fpage\":\"")
-				.append(fpage).append('\"');
-		sb.append(",\"lpage\":\"")
-				.append(lpage).append('\"');
-		sb.append('}');
-		return sb.toString();
+	public JSONObject toJson() {
+		JSONObject jsonObject=new JSONObject();
+		jsonObject.put("id",id);
+		jsonObject.put("label",label);
+		jsonObject.put("authors",TypeConverter.list2JsonArray(authors));
+		jsonObject.put("year",year);
+		jsonObject.put("article_title",article_title);
+		jsonObject.put("source",source);
+		jsonObject.put("volume",volume);
+		jsonObject.put("issue",issue);
+		jsonObject.put("fpage",fpage);
+		jsonObject.put("lpage",lpage);
+    	return jsonObject;
 	}
 }
